1252373655bd9861be2496873ab5503adb61416e
wizzard95 <damian110995a@gmail.com>
2025-08-17 15:45:50 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /* base:'https://wizzard95.github.io/portfolio-website-damian' */
   base: '/portfolio-website-damian/',
 })
