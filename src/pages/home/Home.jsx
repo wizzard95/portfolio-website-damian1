7f9f9d92e3465754337583fd6c900d4208550c89
wizzard95 <damian110995a@gmail.com>
2026-04-07 16:45:07 -0400

    actualizacion de cv

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import Cv from '../../pdf/cv_damian_acuña_ING.pdf';
+import Cv from '../../pdf/cv_damian-acuña2026.pdf';
 import Profile from "../../assets/yo.jpeg"; 
 import { Link } from "react-router";
 import { RiArrowRightLine, RiDownload2Line, RiMailLine, } from "react-icons/ri";
